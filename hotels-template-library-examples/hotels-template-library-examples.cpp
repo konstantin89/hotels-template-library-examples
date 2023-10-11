7ff2bf79a2878e1f0a54401127645182af6b41b7
Kosta Bulgakov <kosta.bulgakov@mail.huji.ac.il>
2023-10-11 14:32:10 +0300

    init

---
+++ hotels-template-library-examples/hotels-template-library-examples.cpp
@@ -0,0 +1,12 @@
+﻿// hotels-template-library-examples.cpp : Defines the entry point for the application.
+//
+
+#include "hotels-template-library-examples.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
